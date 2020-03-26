47716ca57478426fee72cc43889b3c36ddddc852
vpc <cuevasvp@gmail.com>
2020-03-26 15:40:53 +0800

    change examples

--- BaseConversion.py
+++ BaseConversion.py
@@ -1,7 +1,9 @@
-print(bin(123))
-print(hex(123))
+print(bin(15))
+print(hex(15))
 print(int('1111', 2))
+print(int('f', 16))
 
+print('-----------------')
 print(hex(int('1111', 2)))  # binary to hex
 print(bin(int('ff', 16)))  # hex to binary
 print(int('v', 32))  # base must be >= 2 and <= 36
