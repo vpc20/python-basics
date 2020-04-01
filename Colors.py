5726df72e271c19d5cd20e04dfcbf429d9fcaa23
vpc <cuevasvp@gmail.com>
2020-04-01 16:09:37 +0800

    new print color function

--- Colors.py
+++ Colors.py
@@ -11,6 +11,21 @@
 #             print(s1)
 #         print('\n')
 
+WHITE = 30
+RED = 31
+GREEN = 32
+BROWN = 33
+BLUE = 34
+MAGENTA = 35
+CYAN = 36
+GRAY = 37
+BLACK = 37
+
+NORMAL = 0
+BOLD = 1
+ITALIC = 3
+UNDERLINE = 4
+
 
 def print_format_table():
     """
@@ -26,4 +41,13 @@ def print_format_table():
         print('')
 
 
+def print_color(s, style, fg, bg):
+    txt_color = ';'.join([str(style), str(fg), str(bg)])
+    print(f'\x1b[{txt_color}m{s}\x1b[0m')
+
+
 print_format_table()
+print_color("hello", NORMAL, RED, BLUE)
+print_color("hello", BOLD, RED, BLUE)
+print_color("hello", ITALIC, RED, BLUE)
+print_color("hello", UNDERLINE, RED, BLUE)
