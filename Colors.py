71d2a78545c6ff8a7bf295dc4efc212efc1528bd
vpc <cuevasvp@gmail.com>
2020-04-01 16:33:20 +0800

    add reverse display style

--- Colors.py
+++ Colors.py
@@ -11,20 +11,31 @@
 #             print(s1)
 #         print('\n')
 
-WHITE = 30
-RED = 31
-GREEN = 32
-BROWN = 33
-BLUE = 34
-MAGENTA = 35
-CYAN = 36
-GRAY = 37
-BLACK = 37
+FG_WHITE = 30
+FG_RED = 31
+FG_GREEN = 32
+FG_BROWN = 33
+FG_BLUE = 34
+FG_MAGENTA = 35
+FG_CYAN = 36
+FG_GRAY = 37
+FG_BLACK = 38
+
+BG_WHITE = 40
+BG_RED = 41
+BG_GREEN = 42
+BG_BROWN = 43
+BG_BLUE = 44
+BG_MAGENTA = 45
+BG_CYAN = 46
+BG_GRAY = 47
+BG_BLACK = 48
 
 NORMAL = 0
 BOLD = 1
 ITALIC = 3
 UNDERLINE = 4
+REVERSE = 7
 
 
 def print_format_table():
@@ -47,7 +58,24 @@ def print_color(s, style, fg, bg):
 
 
 print_format_table()
-print_color("hello", NORMAL, RED, BLUE)
-print_color("hello", BOLD, RED, BLUE)
-print_color("hello", ITALIC, RED, BLUE)
-print_color("hello", UNDERLINE, RED, BLUE)
+print_color("hello", NORMAL, FG_RED, BG_GRAY)
+print_color("hello", BOLD, FG_RED, BG_GRAY)
+print_color("hello", ITALIC, FG_RED, BG_GRAY)
+print_color("hello", UNDERLINE, FG_RED, BG_GRAY)
+print_color("hello", NORMAL, FG_WHITE, BG_CYAN)
+print_color("hello", REVERSE, FG_RED, BG_BLACK)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
