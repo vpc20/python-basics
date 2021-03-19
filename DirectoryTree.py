d9e1ddf01b43ec76e8827fcc8cfd78b7eb8be66e
vpc <cuevasvp@gmail.com>
2021-03-19 11:47:41 +0800

    fixed

--- DirectoryTree.py
+++ DirectoryTree.py
@@ -2,44 +2,26 @@ import os
 
 
 def print_dir(currpath):
-    def prtdir(path, lvl):
+    def prtdir(path, lvl, lastdirs):
         for dirpath, dirnames, filenames in os.walk(path):
             for dirname in dirnames:
+                pref = ''
+                for lastdir in lastdirs:
+                    if lastdir:
+                        pref += '    '
+                    else:
+                        pref += '│   '
                 if dirname != dirnames[-1]:
-                    if lvl == 0:
-                        print(f'├{"    " * lvl}── {dirname}')
-                    else:
-                        print(f'{"│    " * lvl}├── {dirname}')
+                    print(f'{pref}├── {dirname}')
                 else:
-                    if lvl == 0:
-                        print(f'└{"    " * lvl}── {dirname}')
-                    else:
-                        print(f'{"│    " * lvl}└── {dirname}')
-                # if dirname != dirnames[-1]:
-                #     if lvl == 0:
-                #         print(f'├{"   " * lvl}── {dirname}')
-                #     else:
-                #         print(f'│{"   " * lvl}└── {dirname}')
-                # else:
-                #     if lvl == 0:
-                #         print(f'└── {dirname}')
-                #     else:
-                #         print(f'│{"   " * lvl}└── {dirname}')
-                prtdir(os.path.join(dirpath, dirname), lvl + 1)
+                    print(f'{pref}└── {dirname}')
+                prtdir(os.path.join(dirpath, dirname), lvl + 1, lastdirs + [dirname == dirnames[-1]])
             dirnames.clear()  # we're doing recursion so we only need to process one level directory for os.walk
-            # for filename in filenames:
-            #     if filename != filenames[-1]:
-            #         print(f'   ├{"────" * lvl} {filename}')
-            #     else:
-            #         print(f'   ├{"────" * lvl} {filename}')
-                    # print(f'{"   " * lvl}└─── {filename}')
-                # print(os.path.join(dirpath, filename))
 
     print(currpath)
-    prtdir(currpath, 0)
+    prtdir(currpath, 0, [])
 
 
 # print_dir('c:\\go')
 print_dir(r'c:\\temp')
 # print_dir(r'C:\Users\vpc\PycharmProjects\PythonBasics')
-
