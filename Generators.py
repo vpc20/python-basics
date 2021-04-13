6d1169363de39a50bbd349424c0daef0ed1da239
vpc <cuevasvp@gmail.com>
2021-04-14 05:09:15 +0800

    new example

--- Generators.py
+++ Generators.py
@@ -1,4 +1,3 @@
-
 # x = range(10)
 # print x
 # print type(x)
@@ -7,10 +6,17 @@ def number_generator(n):
     for i in range(n):
         yield i
 
-for num in number_generator(10):
+
+gennum = number_generator(10)
+print(next(gennum))
+print(next(gennum))
+print(next(gennum))
+
+# print the rest
+for num in gennum:
     print(num)
 
 # generator comprehension
 animals = ['dog', 'cat', 'duck']
-for a in  (animal for animal in animals):
+for a in (animal for animal in animals):
     print(a)
