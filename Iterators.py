9ef2402d92649be8b70a55d65d53a625f178c146
vpc <cuevasvp@gmail.com>
2021-04-15 15:40:16 +0800

    new example

--- Iterators.py
+++ Iterators.py
@@ -1,3 +1,31 @@
+class InfiniteCounter:
+
+    def __init__(self):
+        self.ctr = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.ctr += 1
+        return self.ctr
+
+
+# create an object
+infctr = InfiniteCounter()
+
+# create an iterable from the object
+ic = iter(infctr)
+
+# Using next to get to the next iterator element
+print(next(ic))
+print(next(ic))
+print(next(ic))
+print(next(ic))
+print(next(ic))
+
+
+# create iterator from sequence
 iter1 = iter([1, 2, 3, 4, 5])
 
 print(next(iter1))
