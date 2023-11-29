63a783326ffdd2bae49f1f753fbdf18e4427bdde
vpc20 <cuevasvp@yahoo.com>
2023-11-29 09:35:30 +0800

    revision

--- TextFile.py
+++ TextFile.py
@@ -20,6 +20,25 @@ def read_text_file2(filename):
 
 
 # read_text_file1('UnitTest.py')
-read_text_file1(r'c:\users\vpc\share\rockyou.txt')
+
+# read_text_file1(r'c:\users\vpc\share\rockyou.txt')
+
+read_text_file1('timer.py')
 print(f'\n{("=" * 96)}\n')
 read_text_file2('UnitTest.py')
+
+s1 = f"""
+   line1
+      line2
+         line3
+   line1a      
+"""
+
+outf = open('outfile.txt', 'w')
+outf.close()
+
+outf = open('outfile.txt', 'a')
+outf.write(s1)
+outf.close()
+
+
