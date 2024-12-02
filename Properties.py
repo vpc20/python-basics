da5f9d4e86b616cef4002d1229348871e26249d0
vpc20 <cuevasvp@yahoo.com>
2024-12-03 07:26:08 +0800

    additional class

--- Properties.py
+++ Properties.py
@@ -11,6 +11,25 @@ class Person:
         self._name = value
 
 
+class Box:
+    def __init__(self, weight):
+        self.__weight = weight
+
+    @property
+    def weight(self):
+        """Docstring for the 'weight' property"""
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        if weight >= 0:
+            self.__weight = weight
+
+    @weight.deleter
+    def weight(self):
+        del self.__weight
+
+
 me = Person("Alex")
 print(me.name)
 me.name = "Edward"
